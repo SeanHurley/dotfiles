986f8de4f3ac798c430db3fb4da57de983e2ab32
Sean Hurley <sean.hurley6@gmail.com>
2013-11-27 09:17:46 -0600

    Fix git prompt for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 . ~/.aliases
 set fish_greeting ""
 set EDITOR /usr/local/bin/vim
+set DYLD_FALLBACK_LIBRARY_PATH /Applications/Postgres.app/Contents/MacOS/lib:$DYLD_LIBRARY_PATH
+
+set PATH ~/android-sdk-macosx/tools $PATH
+set PATH ~/android-sdk-macosx/platform-tools $PATH
+set PATH ~/packer $PATH
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -10,30 +10,33 @@ function fish_prompt --description 'Write out the prompt'
 
     if test (git rev-parse > /dev/null 2>&1;echo $status) -eq 0
       if not set -q __git_cb
-          set __git_cb ":"(set_color brown)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)""
+          set branch (git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)
+          set __git_cb "@"(set_color brown)"$branch"
       end
 
       if not set -q __git_mod
-          set __git_mod (set_color red)(git ls-files -m ^/dev/null | wc -l)(set_color normal)""
+          set __git_mod (set_color red)(git ls-files -m ^/dev/null | wc -l | sed -e 's/^[ \t]*//')(set_color normal)""
       end
 
       if not set -q __git_unt
-          set __git_unt (set_color yellow)(git ls-files --other --exclude-standard | wc -l)(set_color normal)""
+          set __git_unt (set_color yellow)(git ls-files --other --exclude-standard | wc -l | sed -e 's/^[ \t]*//')(set_color normal)""
       end
 
       if not set -q __git_staged
-          set __git_staged (set_color green)(git diff --name-only --cached | wc -l)(set_color normal)""
+          set __git_staged (set_color green)(git diff --name-only --cached | wc -l | sed -e 's/^[ \t]*//')(set_color normal)""
       end
 
       if not set -q __git_ahead
-          set -lx ahead (git rev-list --left-right origin/HEAD...HEAD | grep \> -c)
+          set -lx remote (git remote show)
+          set -lx ahead (git log --oneline $remote/HEAD.. 2> /dev/null | wc -l | sed -e 's/^[ \t]*//')
           if not test $ahead -eq 0
             set __git_ahead "↑"(set_color white)$ahead(set_color normal)""
           end
       end
 
       if not set -q __git_behind
-          set -lx behind (git rev-list --left-right origin/HEAD...HEAD | grep \< -c)
+          set -lx remote (git remote show)
+          set -lx behind (git log --oneline HEAD..$remote/HEAD 2> /dev/null | wc -l | sed -e 's/^[ \t]*//')
           if not test $behind -eq 0
             set __git_behind "↓"(set_color white)$behind(set_color normal)""
           end

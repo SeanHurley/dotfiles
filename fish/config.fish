33d322841eaf6c410ae0425180212fcaf7ec4c12
Sean Hurley <sean.hurley6@gmail.com>
2014-07-16 12:32:03 -0500

    Fix tmux color scheme

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,4 @@ set DYLD_FALLBACK_LIBRARY_PATH /Applications/Postgres.app/Contents/MacOS/lib:$DY
 set PATH ~/android-sdk-macosx/tools $PATH
 set PATH ~/android-sdk-macosx/platform-tools $PATH
 set PATH ~/packer $PATH
+set TERM screen-256color-bce
